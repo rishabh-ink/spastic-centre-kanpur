984cad46c644d5329728b85c4be4f12106d0a1b4
Rishabh Rao <rishabhsrao@gmail.com>
2013-04-21 20:16:56 +0530

    Moved config.rb to css dir.

--- wp-content/themes/spastic-centre-kanpur/css/config.rb
+++ wp-content/themes/spastic-centre-kanpur/css/config.rb
@@ -1,9 +1,9 @@
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "."
-sass_dir = "./css"
-images_dir = "./img"
-javascripts_dir = "./js"
+css_dir = "../"
+sass_dir = "./"
+images_dir = "../img"
+javascripts_dir = "../js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
