59b8cc3bf009608b152b752b50031632dc36e632
Rishabh Rao <rishabhsrao@gmail.com>
2013-04-27 22:52:37 +0530

    Added featured image support.

--- wp-content/themes/ashayen/functions.php
+++ wp-content/themes/ashayen/functions.php
@@ -3,4 +3,12 @@
     register_nav_menus(array("primary" => "Header navigation"));
   }
 
+  if(function_exists("add_theme_support")) {
+    add_theme_support("post-thumbnails");
+  }
+
+  if(function_exists("add_image_size")) {
+    add_image_size("featured", 640, 480, true);
+    add_image_size("post-thumb", 320, 240, true);
+  }
 ?>
